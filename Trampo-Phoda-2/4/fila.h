8acf9b42d270b8e1b9b7b5a0b947344b35beea24
GabrielVelasco <themrgabriel100@gmail.com>
2022-08-09 17:39:04 -0300

    deleting some useless files

--- Trampo-Phoda-2/4/fila.h
+++
@@ -1,7 +0,0 @@
-typedef struct fila *Fila;
-
-Fila cria_fila();
-int fila_vazia(Fila);
-int insere_cliente(Fila, int, int, int);
-int remove_cliente(Fila, int *, int *, int *);
-int tamanho_fila(Fila);
--- Trampo-Phoda-2/8/deque.h
+++
@@ -1,9 +0,0 @@
-typedef struct deque *Deque;
-
-Deque cria_deque();
-int deque_vazio(Deque);
-int insere_fim(Deque, int);
-int insere_ini(Deque, int);
-int remove_ini(Deque, int *);
-int remove_fim(Deque, int *);
-int libera_deque(Deque);
